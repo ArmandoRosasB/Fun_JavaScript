a544c7f00e5d6f3a4896e752e0e91ae1c0b592b7
ArmandoRosasB <armando.rosas133@gmail.com>
2023-01-03 12:31:09 -0600

    Fun.js

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,9 @@
+// prompt() Permite pedir informacion al usuario en un cuadro de alerta
+const nombre = prompt('Cual es tu nombre?');
+
+// document.querySelector(div) Selecciona el contendo del div en el html
+// .innerHTML Elimina el contenido de ese div del HTML y lo reemplaza
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
+
+
+
---
+++ 01-Introduccion/js/console_commands.js
@@ -0,0 +1,30 @@
+// Limplia la consola
+console.clear();
+
+// Muestra un mensaje en la consola
+console.log([1, 2, 3, 4]);
+
+//Muestra un error en la consola
+console.error("Algo salio mal");
+
+// Muestra una advertencia
+console.warn("Esto es una advertencia");
+
+// Contar tiempo de ejecucion
+
+// Comenzar la cuenta
+console.time("Chrono");
+
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+console.warn("Esto es una advertencia");
+
+// Terminar la cuenta
+console.timeEnd("Chrono");
